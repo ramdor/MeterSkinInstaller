b62e13a91263da766df9f52a5edc37a2d383ec47
Richie <richie_github@grange-lane.co.uk>
2023-02-26 20:19:27 +0000

    Add project files.

---
+++ MeterSkinInstaller/frmMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MeterSkinInstaller
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tmrGo = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Installing Meter Skins";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please wait...";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tmrGo
+            // 
+            this.tmrGo.Enabled = true;
+            this.tmrGo.Interval = 1000;
+            this.tmrGo.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(376, 33);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "from OE3IDE";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 200);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meter Skin Installer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmrGo;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MeterSkinInstaller/frmMain.cs
@@ -0,0 +1,83 @@
+﻿//MW0LGE 26/03/23
+using MeterSkinInstaller.Properties;
+using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Resources;
+using System.Globalization;
+using System.IO;
+
+namespace MeterSkinInstaller
+{
+    public partial class frmMain : Form
+    {
+        private bool _bDone = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(_bDone)
+            {
+                tmrGo.Enabled = false;
+                this.Close();
+                return;
+            }
+
+            tmrGo.Enabled = false;
+
+            Cursor c = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;            
+
+            ResourceSet rsrcSet = Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+
+            if (rsrcSet != null)
+            {
+                string sMeterPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\OpenHPSDR\\Meters";
+
+                bool bOk = true;
+                if (!Directory.Exists(sMeterPath))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(sMeterPath);
+                    }
+                    catch 
+                    {
+                        bOk = false;
+                    }
+                }
+
+                if (bOk)
+                {
+                    foreach (DictionaryEntry entry in rsrcSet)
+                    {
+                        if (entry.Value.GetType() == typeof(Bitmap))
+                        {
+                            Bitmap bmp = entry.Value as Bitmap;
+                            if (bmp != null)
+                            {
+                                //- in filenames become _ in resources, change them back
+                                string sFilename = sMeterPath + "\\" + entry.Key.ToString().Replace('_', '-') + ".png";
+
+                                try
+                                {
+                                    if (File.Exists(sFilename)) File.Delete(sFilename);
+                                    bmp.Save(sFilename, System.Drawing.Imaging.ImageFormat.Png);
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+                }
+            }
+            Cursor = c;
+            _bDone = true;
+            tmrGo.Enabled = true;
+        }
+    }
+}
