a0c34638ede8010b180972dfeab3475badfeae8d
Richie <richie_github@grange-lane.co.uk>
2023-02-28 12:49:46 +0000

    changes to some images

--- MeterSkinInstaller/Properties/Resources.Designer.cs
+++ MeterSkinInstaller/Properties/Resources.Designer.cs
@@ -143,16 +143,6 @@ namespace MeterSkinInstaller.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ananMM_dark_100 {
-            get {
-                object obj = ResourceManager.GetObject("ananMM_dark_100", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap ananMM_dark_large {
             get {
                 object obj = ResourceManager.GetObject("ananMM_dark_large", resourceCulture);
