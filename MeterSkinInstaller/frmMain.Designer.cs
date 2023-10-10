1f756df2897fb9caf1556464a99a8c8ed3dbb50a
Richie <richie_github@grange-lane.co.uk>
2023-10-10 01:44:27 +0100

    turn off taskbar icon

--- MeterSkinInstaller/frmMain.Designer.cs
+++ MeterSkinInstaller/frmMain.Designer.cs
@@ -38,7 +38,7 @@
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 26);
             this.label1.Name = "label1";
@@ -94,6 +94,7 @@
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(400, 200);
             this.Name = "frmMain";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Meter Skin Installer";
             this.TopMost = true;
